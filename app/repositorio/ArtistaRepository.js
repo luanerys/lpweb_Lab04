b3da5a0e60e13a9ef5d4bf954e7e67f2710f8aa1
Mauro Campos <38911137+mauro-pinheiro@users.noreply.github.com>
2020-01-09 14:52:41 -0300

    Ta dando erro

--- app/repositorio/ArtistaRepository.js
+++ app/repositorio/ArtistaRepository.js
@@ -1,36 +1,41 @@
 class ArtistaRepository {
+  constructor(conexao) {
+    this._conexao = conexao;
+  }
 
-    constructor(conexao) {
-        this._conexao = conexao;
+  porId(id, callback) {
+    this._conexao.query(`select * from artistas where id = ${id}`, callback);
+  }
+
+  todos(callback) {
+    this._conexao.query("select * from artistas", callback);
+  }
+
+  salva(artista, callback) {
+    console.log("ID " + artista.id);
+
+    if (artista.hasOwnProperty("id") && artista.id > 0) {
+      this._conexao.query(
+        "update artistas set ? where id = " + artista.id,
+        artista,
+        callback
+      );
+      console.log("executou update");
+    } else {
+      console.log(artista);
+      this._conexao.query("insert into artistas set ?", artista, callback);
+      console.log("executou insert");
     }
+  }
 
-    porId(id, callback) {
-        this._conexao.query(`select * from artistas where id = ${id}`, callback);
-    }
-
-
-    todos(callback) {
-        this._conexao.query('select * from artistas', callback);
-    }
-
-    salva(artista, callback) {
-        console.log('ID ' + artista.id);
-
-        if ((artista.hasOwnProperty('id')) && (artista.id > 0)) {
-            this._conexao.query('update artistas set ? where id = ' + artista.id, artista, callback);
-            console.log('executou update');
-
-        } else {
-            this._conexao.query('insert into artistas set ?', artista, callback);
-            console.log('executou insert');
-
-        }
-    }
-
-    remove(artista, callback) {
-        this._conexao.query('delete from artistas where id = ' + artista.id, callback);
-    }
-
+  remove(artista, callback) {
+    this._conexao.query(
+      "delete from artistas where id = " + artista.id,
+      callback
+    );
+  }
 }
 
-module.exports = () => { return ArtistaRepository };
+module.exports = () => {
+  return ArtistaRepository;
+};
--- app/rotas/rotas.js
+++ app/rotas/rotas.js
@@ -1,25 +1,56 @@
-moment = require('moment');
+let moment = require("moment");
 
 module.exports = function(app) {
-    app.get('/', (req, resp) => resp.render('index'));
+  app.get("/", (req, resp) => resp.render("index"));
 
-    app.get('/musicas/create', (req, resp) => resp.render('musicas/form-musica'));
-    app.get('/artistas/create', (req, resp) => resp.render('artistas/form-artista'));
-    app.get('/albuns/create', (req, resp) => resp.render('albuns/form-album'));
+  app.get("/musicas/create", (req, resp) => resp.render("musicas/form-musica"));
+  app.get("/artistas/create", (req, resp) =>
+    resp.render("artistas/form-artista")
+  );
+  app.get("/albuns/create", (req, resp) => resp.render("albuns/form-album"));
 
+  app.get("/artistas", function(req, resp) {
+    let conexao = new app.infra.ConnectionFactory().getConexao();
+    let artistas = new app.repositorio.ArtistaRepository(conexao);
 
-    app.get('/artistas', function(req, resp) {
-
-        let conexao = new app.infra.ConnectionFactory().getConexao();
-        let artistas = new app.repositorio.ArtistaRepository(conexao);
-
-        artistas.todos(function(erros, resultado) {
-
-            if (erros) {
-                console.log(erros);
-            }
-            resp.render('artistas/lista-artista', { lista: resultado, moment: moment });
-        });
-        conexao.end();
+    artistas.todos(function(erros, resultado) {
+      if (erros) {
+        console.log(erros);
+      }
+      resp.render("artistas/lista-artista", {
+        lista: resultado,
+        moment: moment
+      });
     });
-}
+    conexao.end();
+  });
+
+  app.post("/artistas", function(req, resp) {
+    let artista = req.body;
+    console.log(artista);
+
+    // req.assert("nome", "Nome do artista é obrigatório.").notEmpty();
+    // req.assert("nacionalidade", "Nacionalidade é obrigatório").notEmpty();
+    // req.assert("data_nascimento", "Data inválida").isDate();
+
+    let erros = req.validationErrors();
+
+    if (erros) {
+      resp.render("artistas/form-artista", {
+        errosValidacao: erros,
+        artista: artista
+      });
+      return;
+    }
+
+    let conexao = new app.infra.ConnectionFactory().getConexao();
+    let artistas = new app.repositorio.ArtistaRepository(conexao);
+
+    artistas.salva(artista, function(erros, resultados) {
+      //resp.render('artistas/listagem' );
+      resp.redirect("/artistas");
+    });
+
+    conexao.end();
+  });
+};
